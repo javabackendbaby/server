e898c0a7b1dea5159ebe822f1c688425f610e0bb
javabackendbaby <zapokpupok@gmail.com>
2024-12-15 23:04:54 +0300

    add-dtos

---
+++ src/main/java/com/project/server/dto/JwtAuthenticationResponse.java
@@ -0,0 +1,17 @@
+package com.project.server.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "Ответ c токеном доступа")
+public class JwtAuthenticationResponse {
+    @Schema(description = "Токен доступа", example = "eyJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiUk9MRV9VU...")
+    private String token;
+}
